36a7c9e94e2a960b4fc2f55248063b1757497128
pl0l <141683796+pl0l@users.noreply.github.com>
2023-08-07 17:11:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import React from 'react';
+
 
 function App() {
+  const [Height, SetHeight] = useState(2000);
+  const [Width, SetWidth] = useState(2000);
+  const [XPos, setXPos] = useState(0);
+  const [YPos, setYPos] = useState(0);
+  const canvasRef = useRef(null)
+  useEffect(() => {
+    const canvas = document.getElementById("main-canvas")
+    const ctx = canvas.getContext('2d');
+    var XPosition = 0;
+    var YPosition = 0;
+    var canDraw = false;
+    function handleMouseMove(event) {
+      const rect = canvas.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
+      
+      YPosition = mouseY
+      XPosition = mouseX
+      console.log(XPosition, " " , YPosition)
+      console.log(mouseX, " " , mouseY)
+    }
+
+    function placePixel(event, color){
+      console.log("hi")
+      if (canDraw = true) {
+              // Set the pixel color
+              ctx.fillStyle = "red";
+      
+              ctx.fillRect(XPosition, YPosition, 1, 1);
+      }
+
+    }
+    function handleMouseDown(event, color){
+      canDraw = true;
+      placePixel();
+    }
+    function handleMouseUp(event){
+      canDraw = false;
+    }
+
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mousemove', placePixel);
+    canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousedown', handleMouseUp);
+    return () => {
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener('mousedown', placePixel);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+        <head>
+          <title>Place Pixels!</title>
+        </head>
+        <body>
+          <canvas className="can" id="main-canvas" height={Height} width={Width}></canvas>
+        </body>
+      </html>
   );
 }
 
